5e5ec9c0673383cbd430b7a3594e574bca75248a
Louison <louison59279@gmail.com>
2021-08-20 13:54:43 +0200

    bonne indentation code

--- modal.js
+++ modal.js
@@ -11,23 +11,23 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const modalBody = document.getElementsByClassName("content")[0];
+const bground = document.getElementsByClassName('bground')[0];
+const closeModal = document.getElementsByClassName("close")[0];
+
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
 // launch modal form
 function launchModal() {
-  const modalBody = document.getElementsByClassName("content")[0];
   modalbg.style.display = "block";
   modalBody.style.display = "block";
 };
 
 
 // Close modal
-const closeModal = document.getElementsByClassName("close")[0];
 closeModal.addEventListener('click', function(){
-  const modalBody = document.getElementsByClassName("content")[0];
-  const bground = document.getElementsByClassName('bground')[0];
   modalBody.style.display = "none";
   bground.style.display = 'none';
 });
